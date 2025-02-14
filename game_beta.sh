9928c91527a857eff82ff4696a43749d30bfc3dd
Tyler B. Crawford <164790444+tylerbcrawford@users.noreply.github.com>
2025-02-13 21:59:11 -0500

    Create game_beta.sh

---
+++ game_beta.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# This is a script that simulates the game progress.
+echo "Game started at $(date)" > ~/game_progress.txt
+echo "Player logged in" >> ~/game_progress.txt
+echo "Player completed first level" >> ~/game_progress.txt
